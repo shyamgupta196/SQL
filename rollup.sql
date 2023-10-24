ef6cb9f6926172e55ca9d60e13122b965ae04c10
shyamgupta196 <shyamguptaofficial196@gmail.com>
2023-10-24 11:43:14 +0530

    first commit

---
+++ rollup.sql
@@ -0,0 +1,13 @@
+select
+	cr.country_name, cr.region_name, count(e.*)
+from 
+	data_sci.employees e
+join 
+	data_sci.company_regions cr
+
+on
+	e.region_id = cr.id
+group by 
+	rollup(cr.country_name, cr.region_name)
+order by 
+	cr.country_name, cr.region_name
