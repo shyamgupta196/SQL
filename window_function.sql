7121d43c1e0cb27d94b3db1f994e095168153d4a
shyamgupta196 <shyamguptaofficial196@gmail.com>
2023-10-29 23:48:00 +0530

    not much

--- window_function.sql
+++ window_function.sql
@@ -1,9 +1,33 @@
--- ## partition by is just like group by , its just a way of doing things efficiently.
+-- -- partition by is just like group by , its just a way of doing things efficiently.
+-- -- select 
+-- --   department_id,
+-- --   last_name,
+-- --   salary,
+-- --   first_value(salary)over( partition by department_id order by last_name desc)
+-- -- from 
+-- --   data_sci.employees
+  
+-- -- ALSO 
+-- -- we can see
+-- select 
+--   department_id,
+--   salary,
+--   round(avg(salary) over (partition by department_id),2)
+-- from 
+--    data_sci.employees
+   
+-- ## ntile
+-- select 
+--   department_id,
+--   salary,
+--   ntile(4) over (partition by department_id order by salary desc)
+-- from 
+--    data_sci.employees
+
+-- nth_value 
 select 
   department_id,
-  last_name,
   salary,
-  first_value(salary)over( partition by department_id order by last_name desc)
+  nth_value(salary,2) over (partition by department_id order by salary desc)
 from 
-  data_sci.employees
-  
+   data_sci.employees
